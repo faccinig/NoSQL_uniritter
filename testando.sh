2381a18aa4bf98bc96a29b7244fef0f82a574699
Guilherme Faccini <faccini.g@gmail.com>
2018-09-12 08:55:48 -0300

    init

---
+++ testando.sh
@@ -0,0 +1,21 @@
+mongod --help
+
+mongod --sysinfo
+cd mongoDB
+mongod --dbpath ./data/
+autor
+    "autor",
+    "categoria",
+    "emprestimo",
+    "livro",
+    "livros",
+    "restaurante",
+    "startup_log",
+    "system.indexes"
+
+db.autor.drop()
+db.categoria.drop()
+db.emprestimo.drop()
+db.livro.drop()
+db.livros.drop()
+db.restaurante.drop()
